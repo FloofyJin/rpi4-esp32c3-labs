17a9f81ba3ee34b56819b2a57f66ffbfcc803ed6
FloofyJin <jinsungpark333@gmail.com>
2023-05-02 03:42:54 +0000

    lab3 3 template

--- lab3/lab3_3/main/lab3_3.c
+++ lab3/lab3_3/main/lab3_3.c
@@ -198,6 +198,7 @@ void hid_demo_task(void *pvParameters)
                     vTaskDelay(delays / portTICK_PERIOD_MS);
                 }
             }
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
     }
 }
