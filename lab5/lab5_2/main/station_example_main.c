b2d5c8180877da2b2febbab58161470b0d95c0d7
FloofyJin <jinsungpark333@gmail.com>
2023-05-16 17:25:16 +0000

    before cer changes

--- lab5/lab5_2/main/station_example_main.c
+++ lab5/lab5_2/main/station_example_main.c
@@ -194,11 +194,11 @@ void shtc3_task(){
 //////// start
 // http
 ////////
-#define RPI_SERVER "www.wttr.in"
-#define RPI_PORT "80"
-#define RPI_PATH "/Santa+Cruz?format=%l:+%c+%t"
+#define RPI_SERVER ""
+#define RPI_PORT "8000"
+#define RPI_PATH "/"
 
-static const char *RPI_REQUEST = "put " RPI_PATH " HTTP/1.0\r\n"
+static const char *RPI_REQUEST = "GET " RPI_PATH " HTTP/1.0\r\n"
     "Host: "RPI_SERVER":"RPI_PORT"\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
     "Accept: */*\r\n"
@@ -251,6 +251,101 @@ static void http_get_task(void *pvParameters)
         freeaddrinfo(res);
 
         if (write(s, RPI_REQUEST, strlen(RPI_REQUEST)) < 0) {
+            ESP_LOGE(TAG, "... socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                sizeof(receiving_timeout)) < 0) {
+            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+        /* Read HTTP response */
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+        close(s);
+        for(int countdown = 10; countdown >= 0; countdown--) {
+            ESP_LOGI(TAG, "%d... ", countdown);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        ESP_LOGI(TAG, "Starting again!");
+    }
+}
+
+static const char *RPI_POST_REQUEST = "POST " RPI_PATH " HTTP/1.0\r\n"
+    "Host: "RPI_SERVER":"RPI_PORT"\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "Accept: */*\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: 13\r\n"
+    "\r\n"
+    "Hello, world!"
+    ;
+
+static void http_post_task(void *pvParameters)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while(1) {
+        int err = getaddrinfo(RPI_SERVER, RPI_PORT, &hints, &res);
+
+        if(err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        /* Code to print the resolved IP.
+
+           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket");
+
+        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "... connected");
+        freeaddrinfo(res);
+
+        if (write(s, RPI_POST_REQUEST, strlen(RPI_POST_REQUEST)) < 0) {
             ESP_LOGE(TAG, "... socket send failed");
             close(s);
             vTaskDelay(4000 / portTICK_PERIOD_MS);
@@ -560,6 +655,8 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 
 void wifi_init_sta(void)
 {
+    esp_wifi_sta_wpa2_ent_set_ca_cert(ca_certificate_pem, strlen(ca_certificate_pem));
+
     s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
@@ -649,8 +746,13 @@ void app_main(void)
     // shtc3_task();
     xTaskCreate(&shtc3_task, "shtc3_task", 4096, NULL, 5, NULL);
 
-    // xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
-    xTaskCreate(&https_request_task, "https_get_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+    // xTaskCreate(&https_request_task, "https_get_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&http_get_task, "http_post_task", 4096, NULL, 5, NULL);
 }
 
 // curl "www.wttr.in/Santa+Cruz?format=%l:+%c+%t"
+// curl POST server.py: 
+// curl -v -X POST -H "Content-Type: text/plain" -d 'Hello, world!' http://localhost:8000
+// curl GET server.py:
+// curl -X GET http://localhost:8000
